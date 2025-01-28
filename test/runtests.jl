34c7eec3490cf52ac3118e89a12f1ad5817ad973
Thomas <t.o.c.cheng@student.tudelft.nl>
2025-01-28 23:37:10 +0100

    Re-enabled Aqua and JET

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ using Aqua
 using JET
 
 @testset "GmshReader.jl" begin
-  # @testset "Code quality (Aqua.jl)" begin
-  #   Aqua.test_all(FlatMat)
-  # end
-  # @testset "Code linting (JET.jl)" begin
-  #   JET.test_package(FlatMat; target_defined_modules=true)
-  # end
+  @testset "Code quality (Aqua.jl)" begin
+    Aqua.test_all(FlatMat)
+  end
+  @testset "Code linting (JET.jl)" begin
+    JET.test_package(FlatMat; target_defined_modules=true)
+  end
 
   dir = ""
   if basename(pwd()) == "GmshReader"
